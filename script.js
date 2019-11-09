11d1c0c65d7282ebd211faa8735dd457729e9f45
Serhii Shved <chuhaystyr@gmail.com>
2019-11-09 02:24:11 +0200

    task3

--- script.js
+++ script.js
@@ -16,3 +16,59 @@ function isPrime(num) {
 function factorial(num) {
   return num ? num * factorial(num - 1) : 1
 }
+
+function fib(num) {
+  return num <= 1 ? num : fib(num - 1) + fib(num - 2)
+}
+
+function isSorted(arr) {
+  let origArr = arr.concat()
+  arr.sort((a, b) => a - b)
+  if (origArr.toString() === arr.toString()) {
+    return true
+  }
+  return false
+}
+
+function reverse(str) {
+  let strArr = str.split("")
+  let result = ""
+  for (let i = str.length - 1; i >= 0; --i) {
+    result = result + strArr[i]
+  }
+  return result
+}
+
+function indexOf(arr, value) {
+  for (let i = 0; i < arr.length; ++i) {
+    if (arr[i] === value) {
+      return i
+    }
+  }
+  return -1
+}
+
+function isPalindrome(str) {
+  let origStr = str
+    .toLowerCase()
+    .split(" ")
+    .join("")
+    .split("")
+  let reverseStr = str
+    .toLowerCase()
+    .split(" ")
+    .join("")
+    .split("")
+    .reverse()
+  if (origStr.toString() === reverseStr.toString()) {
+    return true
+  }
+  return false
+}
+
+function missing(arr) {
+  let n = arr.length
+  let totalSum = ((n + 1) * (n + 2)) / 2
+  let arrSum = arr.reduce((sum, current) => sum + current)
+  return totalSum - arrSum
+}
