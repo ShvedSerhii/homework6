4081661450b93aaab4599ab3c63d7e841991094f
Serhii <chuhaystyr@gmail.com>
2019-11-13 00:28:13 +0200

    task2

---
+++ script.js
@@ -0,0 +1,43 @@
+let tags = document.querySelectorAll('.tagging-element')
+for (tag of tags){
+    mouseDown(tag)
+}
+
+function mouseDown(tag) {
+  tag.onmousedown = function(event) {
+    let shiftX = event.clientX - tag.getBoundingClientRect().left;
+    let shiftY = event.clientY - tag.getBoundingClientRect().top;
+
+    tag.style.position = "absolute";
+    tag.style.zIndex = 1000;
+    document.body.append(tag);
+
+    moveAt(event.pageX, event.pageY);
+
+    function moveAt(pageX, pageY) {
+      tag.style.left = pageX - shiftX + "px";
+      tag.style.top = pageY - shiftY + "px";
+    }
+
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+
+      tag.hidden = true;
+      let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+      tag.hidden = false;
+
+      if (!elemBelow) return;
+    }
+
+    document.addEventListener("mousemove", onMouseMove);
+
+    tag.onmouseup = function() {
+      document.removeEventListener("mousemove", onMouseMove);
+      tag.onmouseup = null;
+    };
+  };
+
+  tag.ondragstart = function() {
+    return false;
+  };
+}
