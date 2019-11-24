8d975977d05f83db2477f9cad8e23e1eb123529d
Serhii <chuhaystyr@gmail.com>
2019-11-25 01:46:18 +0200

    gg

--- script.js
+++ script.js
@@ -1,57 +1,75 @@
-let tags = document.querySelectorAll(".tagging-element")
+let tagging = document.querySelector(".tagging");
+const TOP = tagging.getBoundingClientRect().y;
+const LEFT = tagging.getBoundingClientRect().x;
+const BOTTOM =
+  tagging.getBoundingClientRect().y + tagging.getBoundingClientRect().height;
+const RIGHT =
+  tagging.getBoundingClientRect().x + tagging.getBoundingClientRect().width;
+
+let tags = document.querySelectorAll(".tagging-element");
 for (tag of tags) {
-  mouseDown(tag)
-  hidden(tag)
+  mouseDown(tag);
+  hidden(tag);
 }
 
 function hidden(tag) {
-  let hideButton = tag.querySelector("button")
+  let hideButton = tag.querySelector("button");
   tag.onfocus = function() {
-    hideButton.style.visibility = "visible"
-  }
+    hideButton.style.visibility = "visible";
+  };
   tag.onblur = function() {
-    hideButton.style.visibility = "hidden"
-  }
+    hideButton.style.visibility = "hidden";
+  };
   hideButton.onmousedown = function() {
-    tag.style.visibility = "hidden"
-  }
+    tag.style.visibility = "hidden";
+  };
 }
 
 function mouseDown(tag) {
   tag.onmousedown = function(event) {
-    let shiftX = event.clientX - tag.getBoundingClientRect().left
-    let shiftY = event.clientY - tag.getBoundingClientRect().top
+    let shiftX = event.clientX - tag.getBoundingClientRect().left;
+    let shiftY = event.clientY - tag.getBoundingClientRect().top;
 
-    tag.style.position = "absolute"
-    tag.style.zIndex = 1000
-    document.body.append(tag)
+    tag.style.position = "absolute";
+    tag.style.zIndex = 1000;
+    document.body.append(tag);
 
-    moveAt(event.pageX, event.pageY)
+    moveAt(event.pageX, event.pageY);
 
     function moveAt(pageX, pageY) {
-      tag.style.left = pageX - shiftX + "px"
-      tag.style.top = pageY - shiftY + "px"
+      tag.style.left =
+        (pageX - shiftX < LEFT
+          ? LEFT
+          : pageX - shiftX > RIGHT - tag.getBoundingClientRect().width
+          ? RIGHT - tag.getBoundingClientRect().width
+          : pageX - shiftX) + "px";
+      tag.style.top =
+        (pageY - shiftY < TOP
+          ? TOP
+          : pageY - shiftY > BOTTOM - tag.getBoundingClientRect().height
+          ? BOTTOM - tag.getBoundingClientRect().height
+          : pageY - shiftY) + "px";
     }
 
     function onMouseMove(event) {
-      moveAt(event.pageX, event.pageY)
+      moveAt(event.pageX, event.pageY);
 
-      tag.hidden = true
-      let elemBelow = document.elementFromPoint(event.clientX, event.clientY)
-      tag.hidden = false
+      tag.hidden = true;
+      let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+      tag.hidden = false;
 
-      if (!elemBelow) return
+      if (!elemBelow) return;
     }
 
-    document.addEventListener("mousemove", onMouseMove)
+    document.addEventListener("mousemove", onMouseMove);
 
     tag.onmouseup = function() {
       document.removeEventListener("mousemove", onMouseMove);
-      tag.onmouseup = null
-    }
-  }
+      tag.onmouseup = null;
+    };
+  };
 
   tag.ondragstart = function() {
-    return false
-  }
+    return false;
+  };
 }
